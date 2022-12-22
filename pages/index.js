f9cacb41b8b75f9128d9d829c84be3b45d719f86
Arafat Alim <arafat.alim@energport.com>
2022-12-22 13:55:04 -0800

    second

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>HI</h1>;
+  return (
+    <div>
+      <h1>HI</h1>
+      <h2>In order to use this api, add /api/decipher/xxxxxxxx after the url</h2>
+      <h2>replace xxxxxxxx with error code</h2>
+    </div>
+  );
 }
